0eebf159fe430c73285dd209a59b7b2d1ff43d20
OfirGavish <81085873+OfirGavish@users.noreply.github.com>
2025-07-25 00:12:03 +0300

    Fix: Allow anonymous access to API routes for client-side MSAL authentication

--- api/getUsers/index.js
+++ api/getUsers/index.js
@@ -2,6 +2,8 @@ const { callGraphAPI } = require('../shared/graphHelper');
 
 module.exports = async function (context, req) {
     context.log('GetUsers request received');
+    context.log('Request headers:', Object.keys(req.headers));
+    context.log('Has x-delegated-access-token:', !!req.headers['x-delegated-access-token']);
 
     try {
         // Call Microsoft Graph to get users using delegated permissions
@@ -18,8 +20,10 @@ module.exports = async function (context, req) {
 
     } catch (error) {
         context.log('Error in getUsers:', error);
+        context.log('Error details:', error.message);
+        context.log('Error stack:', error.stack);
         context.res = {
-            status: 500,
+            status: 401, // Change to 401 to see if this is where the error comes from
             body: { 
                 error: 'Failed to retrieve users', 
                 details: error.message 
