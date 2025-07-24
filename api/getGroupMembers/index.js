834808f099a86ba500667d71d07783fae606e27d
OfirGavish <81085873+OfirGavish@users.noreply.github.com>
2025-07-25 02:41:27 +0300

    Refine Graph API query to exclude device-specific fields for users/groups

--- api/getGroupMembers/index.js
+++ api/getGroupMembers/index.js
@@ -18,8 +18,9 @@ module.exports = async function (context, req) {
         }
 
         // Get group members from Microsoft Graph using delegated permissions
-        // Include @odata.type to properly identify user/group/device types
-        const graphUrl = `https://graph.microsoft.com/v1.0/groups/${groupId}/members?$select=@odata.type,id,displayName,userPrincipalName,mail,deviceId,operatingSystem`;
+        // Note: We don't include device-specific fields in $select because they don't exist on users/groups
+        // Microsoft Graph will return all available fields for each object type
+        const graphUrl = `https://graph.microsoft.com/v1.0/groups/${groupId}/members?$select=@odata.type,id,displayName,userPrincipalName,mail`;
         const membersData = await callGraphAPI(graphUrl, req, context);
 
         const members = membersData.value.map(member => ({
@@ -28,7 +29,7 @@ module.exports = async function (context, req) {
             displayName: member.displayName,
             userPrincipalName: member.userPrincipalName,
             mail: member.mail,
-            // Include device-specific fields
+            // Include device-specific fields when they exist (devices will have these, users/groups won't)
             deviceId: member.deviceId,
             operatingSystem: member.operatingSystem,
         }));
